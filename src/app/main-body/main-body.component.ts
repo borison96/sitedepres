aab2dfc97a45d7556ad770374594a19a3af51b2e
Dziegain Boris <borisondziegain@hotmail.com>
2020-05-03 15:29:51 +0200

    03/05/2020

--- src/app/main-body/main-body.component.ts
+++ src/app/main-body/main-body.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainBodyComponent implements OnInit {
 
-  cheminImage1:any = "assets/keyboard-561124.png";
-  cheminImage2:any = "assets/password-2781614.png";
-  cheminImage3:any = "assets/keyboard-561124.png";
-  images = [this.cheminImage1, this.cheminImage2, this.cheminImage3];
+  cheminImage1:any = "assets/password-2781614.png";
+  cheminImage2:any = "assets/security.png";
+  cheminImage3:any = "assets/design.png";
+  cheminImage4:any = "assets/www.png";
+  images = [this.cheminImage1, this.cheminImage2, this.cheminImage3, this.cheminImage4];
   constructor() {
   }
 
